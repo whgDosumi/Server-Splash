a33e0fea0d444ef0165bcde9757368aea74a809a
Dominic Lewis <60369937+whgDosumi@users.noreply.github.com>
2023-09-14 18:21:10 -0400

    Reorganize (#2)

--- node/button.js
+++ node/button.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fs = require('fs');
+const util = require('util');
 
+// Function to make text into a 'slug' that's safe for filesystem.
 function slugify(text) {
     return text
         .toLowerCase()
@@ -9,6 +11,7 @@ function slugify(text) {
         .slice(0, 50); // Limit to 50 characters for file system safety
 }
 
+// Define the button
 class Button {
     constructor(text, backgroundColor, imageSrc, link) {
         this.width = "500px";
@@ -21,56 +24,52 @@ class Button {
         this.link = link;
     }
 
-    // Generate a slug from the button's text and save data to a JSON file
-    save() {
-        let slug = slugify(this.text);
-        let filePath = path.join(__dirname, 'buttons', `${slug}.json`);
-        let buttonData = {
-            text: this.text,
-            backgroundColor: this.backgroundColor,
-            imageSrc: this.imageSrc,
-            link: this.link,
-        };
-
-        fs.writeFile(filePath, JSON.stringify(buttonData), (err) => {
-            if (err) {
-                console.error(`Error saving button data: ${err}`);
-            } else {
-                console.log(`Button data saved as ${slug}.json`);
-            }
-        });
+    // save the button to disk.
+    async save() {
+        try {
+            let write_promise = util.promisify(fs.writeFile);
+            let slug = slugify(this.text);
+            let file_path = path.join(__dirname, 'buttons', `${slug}.json`);
+            let button_data = {
+                text: this.text,
+                backgroundColor: this.backgroundColor,
+                imageSrc: this.imageSrc,
+                link: this.link,
+            };
+            await write_promise(file_path, JSON.stringify(button_data));
+            console.log(`Button data saved as ${slug}.json`);
+        } catch (err) {
+            console.error(`Error saving button data: ${err}`);
+        }
     }
 }
 
+// Load buttons from disk, return a table of button objects
 function load_buttons() {
     const buttonsDirectory = path.join(__dirname, 'buttons');
     const buttons = [];
-
     try {
         const buttonFiles = fs.readdirSync(buttonsDirectory);
-
         for (const buttonFile of buttonFiles) {
             if (buttonFile.endsWith('.json')) {
                 const filePath = path.join(buttonsDirectory, buttonFile);
                 const buttonData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-
                 const button = new Button(
                     buttonData.text,
                     buttonData.backgroundColor,
                     buttonData.imageSrc,
                     buttonData.link
                 );
-
                 buttons.push(button);
             }
         }
     } catch (error) {
         console.error(`Error loading buttons: ${error}`);
     }
-
     return buttons;
 }
 
+// Export module items
 module.exports = {
     Button,
     load_buttons,
--- node/main.js
+++ node/main.js
@@ -6,39 +6,47 @@ const { Button, load_buttons } = require("./button");
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, "/uploads") });
 
-if (!fs.existsSync(path.join(__dirname, "/buttons"))) {
-    fs.mkdirSync(path.join(__dirname, "/buttons"));
-}
+// Define static content like images and stylesheets
+app.use(express.static(path.join(__dirname, "/public")));
+// Set our view engine to ejs, and configure where views are located
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+// Function to make text into a 'slug' that's safe for filesystem.
 function slugify(text) {
     return text
         .toLowerCase()
         .replace(/ /g, '-')
         .replace(/[^a-z0-9-]/g, '')
-        .slice(0, 50); // Limit to 50 characters for file system safety
+        .slice(0, 50); // Limit to 50
 }
 
-app.use(express.static(path.join(__dirname, "/public")));
+// Make sure the buttons directory exists
+if (!fs.existsSync(path.join(__dirname, "/buttons"))) {
+    fs.mkdirSync(path.join(__dirname, "/buttons"));
+}
+// Read in the buttons
 let buttons = load_buttons();
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+
+// Handle GET requests
 app.get("/", (req, res) => {
     res.render("index", { buttons });
 });
 app.get("/edit", (req, res) => {
     res.render("edit");
-})
+});
 
+// Handle POST requests
 app.post("/add_button", upload.single("image"), (req, res) => {
-    const text = req.body.text;
-    const color = req.body.color;
-    const link = req.body.link;
-    const ext = "." + req.file.originalname.split('.').pop();
-    const image_path = req.file.path;
+    let text = req.body.text;
+    let color = req.body.color;
+    let link = req.body.link;
+    let ext = "." + req.file.originalname.split('.').pop();
+    let image_path = req.file.path;
     if (!fs.existsSync(path.join(__dirname, "/public/images/"))) {
         fs.mkdirSync(path.join(__dirname, "/public/images/"));
     }
-    const destination = path.join(__dirname, "/public/images/") + slugify(text) + ext;
+    let destination = path.join(__dirname, "/public/images/") + slugify(text) + ext;
     fs.rename(image_path, destination, async (err) => {
         if (err) {
             console.error('Error saving image:', err);
@@ -47,15 +55,13 @@ app.post("/add_button", upload.single("image"), (req, res) => {
             console.log('Image saved successfully');
             res.send('Image uploaded and saved successfully');
             new_button = new Button(text, color, path.join("/images/" + slugify(text) + ext), link);
-            new_button.save();
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            await new_button.save();
             buttons = load_buttons();
         }
     })
-
 })
 
-
+// Start the webserver on port 3000
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
